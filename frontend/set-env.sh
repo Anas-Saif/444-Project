6e52cb6287b3ad7633f57467ff327318dc9476aa
Anas Saif <anas.ee.saif@gmail.com>
2024-11-17 23:46:58 +0300

    editss for deployment

--- frontend/set-env.sh
+++ frontend/set-env.sh
@@ -1,2 +1,10 @@
+#!/bin/sh
+
+# Debug statement
+echo "Running set-env.sh script"
+
 # Set the API_ENDPOINT environment variable
 echo "window.API_ENDPOINT = '${API_ENDPOINT}';" > /usr/share/nginx/html/config.js
+
+# Debug statement to check the content of config.js
+cat /usr/share/nginx/html/config.js
